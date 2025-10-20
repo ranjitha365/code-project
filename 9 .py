5dc716871ede0131ac05a510e955a6501bb3bbed
Ranjitha D <dranjithad071@gmail.com>
2025-10-20 08:24:19 -0700

    Add files via upload

---
+++ 9 .py
@@ -0,0 +1,14 @@
+# string methods
+message = " warning "
+print(message.upper()* 5)
+print(message.lower()* 5)
+print(message.strip()* 5)
+print(message. replace("warning", " thankyou "*5))
+
+#string slicing
+name = "chandan"
+print(name[2:4:7])
+s = "chandan \n is a bad boy"
+print(s)
+s1 = "chandan \t is a good boy"
+print(s1)
